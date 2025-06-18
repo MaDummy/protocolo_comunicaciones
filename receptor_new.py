f3f978478a6a3b2e547bfe4abcaf2ec1f71e155c
Panqueque <108034714+Panquequ3@users.noreply.github.com>
2025-06-18 14:58:46 -0400

    reconstrucción del mensaje

--- receptor_new.py
+++ receptor_new.py
@@ -2,6 +2,9 @@ import socket
 import json
 from utils_new import HOST, PORT
 
+mensajeCompleto = []
+
+# Envío de un mensaje largo a través de paquetes y reconstrucción del mensaje
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
@@ -18,8 +21,16 @@ def main():
                     try:
                         paquete = json.loads(linea.strip())
                         print(f"[{paquete['secuencia']}] -> {paquete['mensaje']}, checksum: {paquete['checksum']})")
+                        mensajeCompleto.insert(paquete['secuencia'], paquete['mensaje'])
+
+                        if paquete['fin_de_paquete'] == "1":
+                            print("listo!\n")
+                            for elem in mensajeCompleto:
+                                print(elem, end="")
                     except json.JSONDecodeError as e:
                         print(f"Error al decodificar JSON: {e}")
 
 if __name__ == "__main__":
     main()
+
+    
