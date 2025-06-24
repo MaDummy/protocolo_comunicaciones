08431640af1c696ba26031e6458ad40ec89d8d29
spartanpotato <107315314+spartanpotato@users.noreply.github.com>
2025-06-24 18:40:26 -0400

    simulacion de corrupcion de paquetes

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ https://docs.google.com/document/d/14M2NY1FqXoXGEV5xdnM0JvOWpwydOYqhahMPnwGBSDE/
 ```cmd
 pip install crcmod
 ```
+
+## Ejecucion
+Use una terminal para ejecutar emisor como python emisor.py <archivo.ext>\
+Donde:
+- archivo.ext: El archivo que desea leer y pasar de emisor a receptor, se aceptan distintos tipos de archivos. EJ: imagen.png
+
+Use una terminal para ejecutar receptor como python receptor.py <archivo_salida.ext> <mostrar texto: 0|1>\
+Donde:
+- archivo_salida.ext: nombre con que desea guardar el archivo que el receptor recibe y reconstruye, asegurese de tener la misma extension usada en el emisor. EJ: copia_imagen.png
+- mostrar texto: Un 0 o 1 indicando si desea que la data recibida en el receptor se imprima en consola, util si tiene un archivo de texto, se recomienda tener en 0 si es cualquier otro tipo de archivo
--- emisor.py
+++ emisor.py
@@ -1,32 +1,90 @@
-import time
+#!/usr/bin/env python3
 import socket
-from utils import Modelo, HOST, PORT, dividir_en_chunks
+import json
+import sys
+import os
+import crcmod
+from time import sleep
+from utils_new import HOST, PORT, leer, CLAVE_CESAR, PROBABILIDAD_ERROR_CHECKSUM, base64, anade_ruido
 
-if __name__ == "__main__":
-    modelo = Modelo()
-    tipo_senal = input("¿Qué tipo de señal deseas usar? (AC/DC): ").strip().upper()
-    if tipo_senal not in ["AC", "DC"]:
-        print("Tipo inválido. Usando AC por defecto.")
-        tipo_senal = "AC"
+crc16 = crcmod.predefined.mkCrcFun('crc-ccitt-false')
 
-    datos_binarios = modelo.app.generar_y_digitalizar_senal(tipo=tipo_senal)
+def enviar_paquete(s, secuencia, longitud_mensaje, fragmento_mensaje, checksum, fin_de_paquete):
+    paquete = {
+        "secuencia": secuencia,
+        "longitud": longitud_mensaje,
+        "mensaje": fragmento_mensaje,
+        "checksum": checksum,
+        "fin_de_paquete": fin_de_paquete
+    }
 
-    print("\n[RESULTADO BINARIO] Primeros 100 bits de la señal digital:")
-    print("".join(str(bit) for bit in datos_binarios[:100]))
+    # Convertir a JSON y agregar fin de linea para simular un archivo de texto
+    json_paquete = json.dumps(paquete) + "\n"
+    s.sendall(json_paquete.encode())
 
-    print(f"\n[INFO] Total de bits generados: {len(datos_binarios)}")
+def cesar_general(data):
+    resultado = bytearray()
+    for b in data:
+        resultado.append((b + CLAVE_CESAR) % 256)
+    return bytes(resultado)
 
-    # Aplicación de la arquitectura de 4 capas
-    modelo = Modelo()
+def main():
+    # Verifica que el codigo se este ejecutando correctamente
+    if len(sys.argv) != 2:
+        print("Uso: python emisor_new.py <nombre_archivo.ext>")
+        sys.exit(1)
 
-    chunks = dividir_en_chunks(datos_binarios, 8)
+    ruta_archivo = sys.argv[1]
+    extension = os.path.splitext(ruta_archivo)[1].lower().replace('.', '')
 
+    try:
+        mensaje = leer(ruta_archivo, extension)
+    except Exception as e:
+        print(f"Error al leer archivo: {e}")
+        sys.exit(1)
+
+    longitud_mensaje = 8
+    
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         print("Conectado al receptor")
 
-        for i, chunk in enumerate(chunks):
-            paquete = modelo.empaquetar(chunk)
-            s.sendall(paquete)
-            print(f"Enviado paquete {i+1}: {chunk}")
-            time.sleep(0.01) # Para evitar JSONDecodeError (más de un paquete siendo enviado a la vez)
+        s_file = s.makefile("r") # archivo de lectura vinculado al socket (para ACK/NACK)
+
+        for i in range(int(len(mensaje) / longitud_mensaje) + 1):
+            secuencia = i
+            fragmento_bytes = mensaje[i*longitud_mensaje:(i+1)*longitud_mensaje].encode('utf-8') # Paso 1: convertir a bytes
+            fragmento_base64 = base64.b64encode(fragmento_bytes) # Paso 2: codificar a base64
+            fragmento_cifrado = cesar_general(fragmento_base64) # Paso 3: cifrar con César
+            checksum = crc16(fragmento_cifrado) # Paso 4: calcular checksum
+            fragmento_a_enviar = base64.b64encode(fragmento_cifrado).decode('utf-8') # Paso 5: codificar a base64 para enviar
+            fin_de_paquete = "1" if (i+1)*longitud_mensaje >= len(mensaje) else "0"
+            
+            # Se envía el paquete y se espera confirmación antes de seguir
+            while True:
+                enviar_paquete(s, secuencia, longitud_mensaje, fragmento_a_enviar, checksum, fin_de_paquete)
+
+                """ VALIDANDO CONFIRMACIÓN """
+
+                # Leer la respuesta del receptor
+                respuesta = s_file.readline()
+
+                if not respuesta:
+                    print("no se recibió confirmación")
+                    break # hay que decidir que hacer en este caso creo..
+                try:
+                    confirmacion = json.loads(respuesta.strip())
+                    print(f"[estado: {confirmacion['tipo']} | secuencia: {confirmacion['secuencia']}]")
+
+                    if confirmacion["tipo"] == "ACK":
+                        break  # Se sigue enviando paquetes
+                    elif confirmacion["tipo"] == "NAK":
+                        print("Reenviando... secuencia:", confirmacion["secuencia"])
+
+                except json.JSONDecodeError as e:
+                    print(f"Error al decodificar JSON: {e}")
+
+
+            
+if __name__ == "__main__":
+    main()
